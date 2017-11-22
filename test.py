8b93f86c89b6eb1d30580a97d794bbff023e4783
정현호 <gudjsgh6@gmail.com>
2017-11-22 14:38:38 +0900

    배경 이미지 추가

--- test.py
+++ test.py
@@ -15,6 +15,12 @@ y = 30
 
 clock = pygame.time.Clock()
 
+myImg = pygame.image.load('images/background.jpg')
+
+def myimg(x, y):
+    screen.blit(myImg, (x, y))
+
+
 while not finish:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -29,12 +35,14 @@ while not finish:
         color = (255, 0, 0)
 
     pressed = pygame.key.get_pressed()
-    if pressed[pygame.K_UP] : y -= 3
-    if pressed[pygame.K_DOWN] : y += 3
-    if pressed[pygame.K_LEFT] : x -= 3
-    if pressed[pygame.K_RIGHT] : x += 3
-    screen.fill((0,0,0)) # 화면 지우기
-
+    if pressed[pygame.K_UP]: y -= 3
+    if pressed[pygame.K_DOWN]: y += 3
+    if pressed[pygame.K_LEFT]: x -= 3
+    if pressed[pygame.K_RIGHT]: x += 3
+    screen.fill((0, 0, 0))  # 화면 지우기
+    myimg(0, 0) # 배경 이미지 삽입
     pygame.draw.rect(screen, color, pygame.Rect(x, y, 60, 60))
     pygame.display.flip()
     clock.tick(60)
+pygame.quit()
+quit()
